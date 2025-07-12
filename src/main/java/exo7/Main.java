cdcc589eebd3bc3886bf0e038b3628f090720681
colombefioren <colomberakotonjanahary@gmail.com>
2025-07-12 16:18:06 +0300

    feat: main class for verification

--- src/main/java/exo7/Main.java
+++ src/main/java/exo7/Main.java
@@ -2,7 +2,20 @@ package exo7;
 
 public class Main {
   public static void main(String[] args) {
-    Cloth cloth = new Cloth("Dress", 2000, 1, Size.XS, "red", Fabric.CUIR);
+
+    // Cloth
+    Cloth cloth = new Cloth("Dress", 2000, 3, Size.XS, "red", Fabric.CUIR);
     System.out.println(cloth);
+    System.out.println(cloth.calculateStockValue());
+
+    // Electronic
+    Electronic electronic = new Electronic("Phone", 1000000, 1, "Apple", 12);
+    System.out.println(electronic);
+    System.out.println(electronic.calculateStockValue());
+
+    // Book
+    Book book = new Book("A Court Of Thorns And Roses", 500, 5, "Sarah J. Maas", 256);
+    System.out.println(book);
+    System.out.println(book.calculateStockValue());
   }
 }
