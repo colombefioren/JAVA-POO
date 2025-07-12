a657a773d2bb88903aa9e95a801652042f8d8c51
colombefioren <colomberakotonjanahary@gmail.com>
2025-07-12 17:29:50 +0300

    feat: search related method in Brasserie

--- src/main/java/exo5/Brasserie.java
+++ src/main/java/exo5/Brasserie.java
@@ -1,6 +1,7 @@
 package exo5;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,4 +13,20 @@ public class Brasserie {
   private List<Boisson> listeBoisson;
   private List<Client> listeClient;
   private List<Commande> listeCommande;
+
+  public List<Boisson> rechercheBoisson(String mot) {
+    return listeBoisson.stream()
+        .filter(b -> b.getNom().toLowerCase().contains(mot.toLowerCase()))
+        .collect(Collectors.toList());
+  }
+
+  public List<Client> rechercheClient(String mot) {
+    return listeClient.stream()
+        .filter(
+            c ->
+                (c.getNom().toLowerCase().contains(mot.toLowerCase())
+                    || (c.getPrenom().toLowerCase().contains(mot.toLowerCase()))
+                    || (c.getEmail().toLowerCase().contains(mot.toLowerCase()))))
+        .collect(Collectors.toList());
+  }
 }
